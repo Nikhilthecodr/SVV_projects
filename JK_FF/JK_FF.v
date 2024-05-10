18ff58a673703c18c9786973605f59fcb96637da
Nikhilthecodr <111330348+Nikhilthecodr@users.noreply.github.com>
2024-05-10 19:46:54 +0530

    Update JK_FF.v

--- JK_FF/JK_FF.v
+++ JK_FF/JK_FF.v
@@ -1,13 +1,14 @@
 module JK_FF(JK_inter.RTL r1);
-  always @ (posedge clk, posedge reset)
+  always @ (posedge r1.clk or posedge r1.reset)
     begin 
       if (r1.reset)
-        r1.q<=0;
+        r1.q <= 0;
       else
-        case ({J,K})
+        case ({r1.J, r1.K})
           2'b00: r1.q <= r1.q;
-          2'b01: r1.q <= 0;
-          2'b10: r1.q <= 1;
-          2'b11: r1.q <= ~(r1.q);
+          2'b01: r1.q <= 1'b0;
+          2'b10: r1.q <= 1'b1;
+          2'b11: r1.q <= ~r1.q;
         endcase
+    end
 endmodule
