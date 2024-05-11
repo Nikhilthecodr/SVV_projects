49b444f5fa1ebd79fe3cc7992d08265879356b08
Nikhilthecodr <111330348+Nikhilthecodr@users.noreply.github.com>
2024-05-11 20:49:55 +0530

    Delete JK_FF directory

--- JK_FF/JK_inter.sv
+++
@@ -1,9 +0,0 @@
-// Code your design here
-interface JK_inter(input bit clk);
-  logic J,K,reset,q;
-  modport RTL(input J,K,reset,clk,output q);
-  modport TB(input q, output J,K,reset);
-endinterface
-
-          
-      
